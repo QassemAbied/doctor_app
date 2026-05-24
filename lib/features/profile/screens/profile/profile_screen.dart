1c8c75e599e1d0751f1524cab43cdf7914e87399
qassem abied <qassemab1@gmail.com>
2026-05-24 21:27:50 +0300

    add profile screen

---
+++ lib/features/profile/screens/profile/profile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:doctor_app/features/profile/screens/profile/profile_body.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
+      body: ProfileBody(),
+    );
+  }
+}
