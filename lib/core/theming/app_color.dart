4933cc334e776113e3f7935da41019adf89c3bca
qassem abied <qassemab1@gmail.com>
2026-05-24 21:23:30 +0300

    update app color palette with primary, grey, surface, and warning colors

--- lib/core/theming/app_color.dart
+++ lib/core/theming/app_color.dart
@@ -1,9 +1,52 @@
-import 'dart:ui';
+
+
+import 'package:flutter/material.dart';
 
 class ColorManager {
-  static const Color mainBlueColor=Color(0xff247cff);
-  static const Color geryColor=Color(0xff757575);
-  static const Color lightGeryColor=Color(0xffc2c2c2);
-  static const Color lighterGeryColor=Color(0xffEDEDED);
-  static const Color blackColor=Color(0xff242424);
+
+
+  static const Color primaryColor = Color(0xFF247CFF);
+  static const Color primary80 = Color(0xFF5095FF);
+  static const Color primary60 = Color(0xFF76A7F5);
+  static const Color primary40 = Color(0xFFD3E5FF);
+  static const Color primary20 = Color(0xFFEAf2FF);
+
+
+  static const Color blackColor = Color(0xFF181A1E);
+
+  static const Color grey100 = Color(0xFF242424);
+  static const Color grey90 = Color(0xFF404040);
+  static const Color grey80 = Color(0xFF616161);
+  static const Color grey70 = Color(0xFF757575);
+  static const Color grey60 = Color(0xFF9E9E9E);
+  static const Color grey50 = Color(0xFFC2C2C2);
+  static const Color grey40 = Color(0xFFE0E0E0);
+  static const Color grey30 = Color(0xFFEDEDED);
+  static const Color grey20 = Color(0xFFF5F5F5);
+
+
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color bodyColor = Color(0xFFFAFAFA);
+
+
+  static const Color surfaceBlue = Color(0xFFEAF2FF);
+  static const Color surfaceGreen = Color(0xFFEAFCEF);
+  static const Color surfaceRed = Color(0xFFFEF4F5);
+
+
+  static const Color fillBlue = Color(0xFF247CFF);
+  static const Color fillGreen = Color(0xFF22C55E);
+  static const Color fillRed = Color(0xFFFF4C5E);
+
+
+  static const Color warningColor = Color(0xFFFACC15);
+  static const Color warning80 = Color(0xFFF9D60A);
+  static const Color warning60 = Color(0xFFF7E16B);
+  static const Color warning40 = Color(0xFFFEEF95);
+  static const Color warning20 = Color(0xFFFFF8D0);
+
+
+  static const Color textPrimary = Color(0xFF242424);
+  static const Color textSecondary = Color(0xFF757575);
+  static const Color textHint = Color(0xFF9E9E9E);
 }
