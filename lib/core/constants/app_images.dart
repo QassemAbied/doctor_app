6d6f21f95afebe15ba255749a3c8073f6867c90f
qassem abied <qassemab1@gmail.com>
2026-05-26 04:53:26 +0300

    add app images and svg constants classes

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,26 @@
+class AppImages {
+  static const String avatar = 'assets/images/avater.png';
+
+  static const String personal = 'assets/images/personal.png';
+
+  static const String banner = 'assets/images/Banner.png';
+
+  static const String call = 'assets/images/call.png';
+
+  static const String doctorImage = 'assets/images/doctor_image.png';
+
+  static const String onboardingDoctor1 =
+      'assets/images/onboarding_doctor_image.png';
+
+  static const String onboardingDoctor2 =
+      'assets/images/onboarding_doctor_image2.jpg';
+
+  static const String onboardingDoctor3 =
+      'assets/images/onboarding_doctor_image3.jpg';
+
+  static const String splash = 'assets/images/Splash.png';
+
+  static const String splash1 = 'assets/images/splash1.png';
+
+  static const String video = 'assets/images/video.png';
+}
---
+++ lib/core/constants/app_svgs.dart
@@ -0,0 +1,41 @@
+class AppSvgs {
+  static const String chat = 'assets/svgs/chat.svg';
+
+  static const String doctorIcon = 'assets/svgs/doctor_icon.svg';
+
+  static const String doneIcon = 'assets/svgs/doneIcon.svg';
+
+  static const String group4778 = 'assets/svgs/Group1000004778.svg';
+
+  static const String group4780 = 'assets/svgs/Group 1000004780.svg';
+
+  static const String group4781 = 'assets/svgs/Group 1000004781.svg';
+
+  static const String home = 'assets/svgs/home-2.svg';
+
+  static const String icon = 'assets/svgs/Icon.svg';
+
+  static const String iconApp = 'assets/svgs/icon_app.svg';
+
+  static const String iconAppOpacity = 'assets/svgs/icon_app_opacity.svg';
+
+  static const String logout = 'assets/svgs/logout_icon.svg';
+
+  static const String mastercard = 'assets/svgs/mastercard.svg';
+
+  static const String paypal = 'assets/svgs/paypal.svg';
+
+  static const String message = 'assets/svgs/message.svg';
+
+  static const String messageText = 'assets/svgs/message-text.svg';
+
+  static const String myAppointment = 'assets/svgs/my_appointment_icon.svg';
+
+  static const String notification = 'assets/svgs/notification.svg';
+
+  static const String personal = 'assets/svgs/personal_icon.svg';
+
+  static const String search = 'assets/svgs/search.svg';
+
+  static const String sort = 'assets/svgs/sort.svg';
+}
