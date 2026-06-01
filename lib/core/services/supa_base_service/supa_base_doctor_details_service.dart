efa9a0fc43cd9fa9f5d08f600403d10ca2a067de
qassem abied <qassemab1@gmail.com>
2026-06-01 18:46:39 +0300

    add doctor details service to fetch doctor by id

---
+++ lib/core/services/supa_base_service/supa_base_doctor_details_service.dart
@@ -0,0 +1,27 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../../../features/home/data/models/doctor_model.dart';
+
+class SupABaseDoctorDetailsService {
+  final instance = Supabase.instance.client;
+
+  Future<DoctorModel?> getDoctorById(String id) async {
+    final response = await instance
+        .from('doctors')
+        .select('''
+  *,
+  specializations(
+    name,
+    image
+  )
+''')
+        .eq('id', id)
+        .maybeSingle();
+    print('response getDoctorById $response');
+
+    if (response == null) {
+      return null;
+    }
+
+    return DoctorModel.fromJson(response);
+  }
+}
