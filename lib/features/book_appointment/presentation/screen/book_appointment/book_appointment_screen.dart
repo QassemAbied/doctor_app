6d39d693ad10aa0ba39cb71fe9c17b0dc82d1985
qassem abied <qassemab1@gmail.com>
2026-06-04 00:21:28 +0300

    add book appointment screen with stepper and page view for appointment steps

---
+++ lib/features/book_appointment/presentation/screen/book_appointment/book_appointment_screen.dart
@@ -0,0 +1,45 @@
+import 'package:doctor_app/core/utils/spacing.dart';
+import 'package:doctor_app/features/book_appointment/presentation/screen/book_appointment/widget/appointment_payment.dart';
+import 'package:doctor_app/features/book_appointment/presentation/screen/book_appointment/widget/appointment_summary.dart';
+import 'package:doctor_app/features/book_appointment/presentation/screen/book_appointment/widget/appointments_date.dart';
+import 'package:doctor_app/features/book_appointment/presentation/screen/book_appointment/widget/stepper_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../home/domain/entities/doctor_entity.dart';
+import '../../controller/book_appointment_cubit.dart';
+
+class BookAppointmentScreen extends StatelessWidget {
+  final DoctorEntity? dataForDoctors;
+  const BookAppointmentScreen({super.key, this.dataForDoctors});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Book Appointment')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            StepperWidget(),
+            verticalSpace(20),
+            Expanded(
+              child: PageView(
+                controller: context.read<BookAppointmentCubit>().pageController,
+                onPageChanged: (value) {
+                  context.read<BookAppointmentCubit>().selectTapBar(value);
+
+                },
+                physics:  BouncingScrollPhysics(),
+                children: [
+                  AppointmentsDate(),
+                  AppointmentPayments(),
+                  AppointmentSummary(dataForDoctors: dataForDoctors),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
