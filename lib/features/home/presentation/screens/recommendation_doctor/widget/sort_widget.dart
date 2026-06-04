782f3cb984864e7b81a4dccd068c625fb34d8a0f
qassem abied <qassemab1@gmail.com>
2026-06-04 04:17:24 +0300

    update sort widget to use recommendation cubit for doctor filtering

--- lib/features/home/presentation/screens/recommendation_doctor/widget/sort_widget.dart
+++ lib/features/home/presentation/screens/recommendation_doctor/widget/sort_widget.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_app/features/home/presentation/controller/recommendation/recommendation_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,120 +7,257 @@ import '../../../../../../core/theming/app_color.dart';
 import '../../../../../../core/theming/app_styles.dart';
 import '../../../../../../core/utils/extension.dart';
 import '../../../../../../core/utils/spacing.dart';
-import '../../../controller/doctor/doctors_cubit.dart';
-import '../../../controller/specialization/specialization_cubit.dart';
-import '../../../controller/specialization/specialization_state.dart';
+import '../../../controller/recommendation/recommendation_state.dart';
 
+// void showFilterBottomSheet(BuildContext context) {
+//
+//   showModalBottomSheet(
+//     context: context,
+//     isScrollControlled: true,
+//     backgroundColor: Colors.transparent,
+//     builder: (_) {
+//       return Container(
+//         padding: const EdgeInsets.all(24),
+//
+//         decoration: const BoxDecoration(
+//           color: ColorManager.whiteColor,
+//
+//           borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
+//         ),
+//
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//
+//           children: [
+//             Center(
+//               child: Text(
+//                 'Sort By',
+//
+//                 style: AppTextStyle.styleBold24(
+//                   ColorManager.textPrimary,
+//                   context,
+//                 ),
+//               ),
+//             ),
+//
+//             verticalSpace(24),
+//
+//             Divider(),
+//
+//             verticalSpace(24),
+//
+//             Text(
+//               'Speciality',
+//
+//               style: AppTextStyle.styleSemiBold18(
+//                 ColorManager.textPrimary,
+//                 context,
+//               ),
+//             ),
+//
+//             verticalSpace(16),
+//
+//             SizedBox(
+//               height: 45,
+//
+//               child: BlocBuilder<RecommendationCubit, RecommendationState>(
+//                 builder: (context, state) {
+//                   final recommendationCubit = context.read<RecommendationCubit>();
+//
+//                   return ListView.builder(
+//                     scrollDirection: Axis.horizontal,
+//
+//                     itemCount: recommendationCubit.categories.length + 1,
+//
+//                     itemBuilder: (context, index) {
+//                       if (index == 0) {
+//                         return filterChip(
+//                           title: 'All',
+//
+//                           selected: recommendationCubit.selectIndex == 0,
+//
+//                           onSelected: () {
+//                             recommendationCubit.changeIndex(0);
+//
+//                             recommendationCubit.filterDoctorsByCategory('All');
+//                             context.pop();
+//                           },
+//                           context: context,
+//                         );
+//                       }
+//
+//                       final category =
+//                       recommendationCubit.categories[index - 1];
+//
+//                       return filterChip(
+//                         title: category.name,
+//
+//                         selected: recommendationCubit.selectIndex == index,
+//
+//                         onSelected: () {
+//                           recommendationCubit.changeIndex(index);
+//
+//                           recommendationCubit.filterDoctorsByCategory(category.id);
+//                           context.pop();
+//                         },
+//                         context: context,
+//                       );
+//                     },
+//                   );
+//                 },
+//               ),
+//             ),
+//
+//             const SizedBox(height: 28),
+//
+//             CustomElevatedButton(
+//               buttonName: 'Done',
+//               onPressed: () {
+//                 context.pop();
+//               },
+//             ),
+//           ],
+//         ),
+//       );
+//     },
+//   );
+// }
 void showFilterBottomSheet(BuildContext context) {
-  final doctorCubit = context.read<DoctorCubit>();
+  final recommendationCubit = context.read<RecommendationCubit>();
 
-  final specializationCubit = context.read<SpecializationCubit>();
   showModalBottomSheet(
     context: context,
+
     isScrollControlled: true,
+
     backgroundColor: Colors.transparent,
+
     builder: (_) {
-      return Container(
-        padding: const EdgeInsets.all(24),
+      return BlocProvider.value(
+        value: recommendationCubit,
 
-        decoration: const BoxDecoration(
-          color: ColorManager.whiteColor,
+        child: Builder(
+          builder: (context) {
+            return Container(
+              padding: const EdgeInsets.all(24),
 
-          borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
-        ),
+              decoration: const BoxDecoration(
+                color: ColorManager.whiteColor,
 
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-
-          crossAxisAlignment: CrossAxisAlignment.start,
-
-          children: [
-            Center(
-              child: Text(
-                'Sort By',
-
-                style: AppTextStyle.styleBold24(
-                  ColorManager.textPrimary,
-                  context,
-                ),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
               ),
-            ),
 
-            verticalSpace(24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
 
-            Divider(),
+                crossAxisAlignment: CrossAxisAlignment.start,
 
-            verticalSpace(24),
+                children: [
+                  Center(
+                    child: Text(
+                      'Sort By',
 
-            Text(
-              'Speciality',
+                      style: AppTextStyle.styleBold24(
+                        ColorManager.textPrimary,
 
-              style: AppTextStyle.styleSemiBold18(
-                ColorManager.textPrimary,
-                context,
-              ),
-            ),
+                        context,
+                      ),
+                    ),
+                  ),
 
-            verticalSpace(16),
+                  verticalSpace(24),
 
-            SizedBox(
-              height: 45,
+                  const Divider(),
 
-              child: BlocBuilder<SpecializationCubit, SpecializationState>(
-                builder: (context, state) {
-                  return ListView.builder(
-                    scrollDirection: Axis.horizontal,
+                  verticalSpace(24),
 
-                    itemCount: specializationCubit.categories.length + 1,
+                  Text(
+                    'Speciality',
 
-                    itemBuilder: (context, index) {
-                      if (index == 0) {
-                        return filterChip(
-                          title: 'All',
+                    style: AppTextStyle.styleSemiBold18(
+                      ColorManager.textPrimary,
 
-                          selected: specializationCubit.selectIndex == 0,
+                      context,
+                    ),
+                  ),
 
-                          onSelected: () {
-                            specializationCubit.changeIndex(0);
+                  verticalSpace(16),
 
-                            doctorCubit.filterDoctorsByCategory('All');
-                            context.pop();
+                  SizedBox(
+                    height: 45,
+
+                    child:
+                        BlocBuilder<RecommendationCubit, RecommendationState>(
+                          builder: (context, state) {
+                            return ListView.builder(
+                              scrollDirection: Axis.horizontal,
+
+                              itemCount:
+                                  recommendationCubit.categories.length + 1,
+
+                              itemBuilder: (context, index) {
+                                if (index == 0) {
+                                  return filterChip(
+                                    title: 'All',
+
+                                    selected:
+                                        recommendationCubit.selectIndex == 0,
+
+                                    onSelected: () {
+                                      recommendationCubit.changeIndex(0);
+
+                                      recommendationCubit
+                                          .filterDoctorsByCategory('All');
+
+                                      context.pop();
+                                    },
+
+                                    context: context,
+                                  );
+                                }
+
+                                final category =
+                                    recommendationCubit.categories[index - 1];
+
+                                return filterChip(
+                                  title: category.name,
+
+                                  selected:
+                                      recommendationCubit.selectIndex == index,
+
+                                  onSelected: () {
+                                    recommendationCubit.changeIndex(index);
+
+                                    recommendationCubit.filterDoctorsByCategory(
+                                      category.id,
+                                    );
+
+                                    context.pop();
+                                  },
+
+                                  context: context,
+                                );
+                              },
+                            );
                           },
-                          context: context,
-                        );
-                      }
+                        ),
+                  ),
 
-                      final category =
-                          specializationCubit.categories[index - 1];
+                  verticalSpace(28),
 
-                      return filterChip(
-                        title: category.name,
+                  CustomElevatedButton(
+                    buttonName: 'Done',
 
-                        selected: specializationCubit.selectIndex == index,
-
-                        onSelected: () {
-                          specializationCubit.changeIndex(index);
-
-                          doctorCubit.filterDoctorsByCategory(category.id);
-                          context.pop();
-                        },
-                        context: context,
-                      );
+                    onPressed: () {
+                      context.pop();
                     },
-                  );
-                },
+                  ),
+                ],
               ),
-            ),
-
-            const SizedBox(height: 28),
-
-            CustomElevatedButton(
-              buttonName: 'Done',
-              onPressed: () {
-                context.pop();
-              },
-            ),
-          ],
+            );
+          },
         ),
       );
     },
