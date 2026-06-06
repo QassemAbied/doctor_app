ad7c2cc7bddcd72472fbb2079c239c4abc7fde53
qassem abied <qassemab1@gmail.com>
2026-06-07 00:43:32 +0300

    refactor doctor data widget and update tab bar styling in details body

--- lib/features/details_doctor/presentation/screen/details_body.dart
+++ lib/features/details_doctor/presentation/screen/details_body.dart
@@ -1,15 +1,13 @@
 import 'package:doctor_app/core/utils/app_router/routes.dart';
 import 'package:doctor_app/core/utils/extension.dart';
 import 'package:doctor_app/features/details_doctor/presentation/screen/widgets/about_tab_bar_widget.dart';
-import 'package:doctor_app/features/details_doctor/presentation/screen/widgets/doctor_data.dart';
+import 'package:doctor_app/core/common_widgets/custom_doctor_data.dart';
 import 'package:doctor_app/features/details_doctor/presentation/screen/widgets/doctor_location_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 import '../../../../core/common_widgets/custom_elevated_botton.dart';
 import '../../../../core/common_widgets/dummy_data.dart';
-import '../../../../core/theming/app_color.dart';
-import '../../../../core/theming/app_styles.dart';
 import '../../../../core/utils/spacing.dart';
 import '../controller/doctor_details_cubit.dart';
 import '../controller/doctor_details_state.dart';
@@ -35,25 +33,9 @@ class DetailsBody extends StatelessWidget {
 
             child: Column(
               children: [
-                DoctorsDataWidget(doctor: doctor),
+                CustomDoctorsDataWidget(doctor: doctor),
 
                 TabBar(
-                  indicatorColor: Colors.transparent,
-                  labelColor: ColorManager.primaryColor,
-                  unselectedLabelColor: ColorManager.grey80,
-                  labelStyle: AppTextStyle.styleBold16(
-                    ColorManager.primaryColor,
-                    context,
-                  ),
-                  unselectedLabelStyle: AppTextStyle.styleMedium16(
-                    ColorManager.grey80,
-                    context,
-                  ),
-                  indicatorWeight: 3,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  labelPadding: const EdgeInsets.symmetric(horizontal: 16),
-                  dividerColor: ColorManager.grey50,
-
                   tabs: [
                     Tab(text: 'About'),
                     Tab(text: 'Location'),
