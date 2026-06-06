a8c2d9a3d98e31ba99e9e422370be92d2ef49c25
qassem abied <qassemab1@gmail.com>
2026-06-07 00:45:49 +0300

    add get, reschedule, and cancel appointment methods to repository

--- lib/features/book_appointment/data/repository_impl.dart
+++ lib/features/book_appointment/data/repository_impl.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:doctor_app/features/book_appointment/domain/entities/appointment_params.dart';
 import '../../../core/error/error.dart';
+import '../domain/entities/appointment_entity.dart';
+import '../domain/mapper/appointment_mapper.dart';
 import '../domain/repository.dart';
 import 'data_source/remote_data_source/appointment_remote_data_source.dart';
 
@@ -8,9 +10,41 @@ class AppointmentRepositoryImpl implements AppointmentRepository {
   final AppointmentRemoteDataSource appointmentRemoteDataSource;
   AppointmentRepositoryImpl(this.appointmentRemoteDataSource);
   @override
-  Future<Either<Failure, void>> bookAppointment(AppointmentParams params) async {
+  Future<Either<Failure, void>> bookAppointment(
+    AppointmentParams params,
+  ) async {
     try {
       await appointmentRemoteDataSource.createAppointment(params);
+      return const Right(null);
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<AppointmentEntity>>> getAppointment() async {
+    try {
+      final appointments = await appointmentRemoteDataSource.getAppointment();
+      return Right(AppointmentMapper.toEntityList(appointments));
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> rescheduleAppointment(RescheduleAppointmentParams params) async {
+    try {
+      await appointmentRemoteDataSource.rescheduleAppointment(params);
+      return const Right(null);
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> cancelAppointment(String id) async {
+    try {
+      await appointmentRemoteDataSource.cancelAppointment(id);
       return const Right(null);
     } catch (e) {
       return Left(Failure(e.toString()));
--- lib/features/book_appointment/domain/repository.dart
+++ lib/features/book_appointment/domain/repository.dart
@@ -1,8 +1,13 @@
 import 'package:dartz/dartz.dart';
+import 'package:doctor_app/features/book_appointment/domain/entities/appointment_entity.dart';
 
 import '../../../core/error/error.dart';
 import 'entities/appointment_params.dart';
 
 abstract class AppointmentRepository {
   Future<Either<Failure, void>> bookAppointment(AppointmentParams params);
+  Future<Either<Failure, List<AppointmentEntity>>> getAppointment();
+  Future<Either<Failure, void>> rescheduleAppointment(RescheduleAppointmentParams params);
+  Future<Either<Failure, void>> cancelAppointment(String id);
+
 }
