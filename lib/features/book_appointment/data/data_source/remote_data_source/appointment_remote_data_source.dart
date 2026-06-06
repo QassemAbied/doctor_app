f7a2527fc1937a9223863dfa301c60f67e4a71bb
qassem abied <qassemab1@gmail.com>
2026-06-07 00:39:32 +0300

    add get, reschedule, and cancel appointment methods to remote data source

--- lib/features/book_appointment/data/data_source/remote_data_source/appointment_remote_data_source.dart
+++ lib/features/book_appointment/data/data_source/remote_data_source/appointment_remote_data_source.dart
@@ -1,5 +1,9 @@
 import '../../../domain/entities/appointment_params.dart';
+import '../../model/appointment_model.dart';
 
 abstract class AppointmentRemoteDataSource {
   Future<void> createAppointment(AppointmentParams params) ;
+  Future<List<AppointmentModel>> getAppointment() ;
+  Future<void> rescheduleAppointment(RescheduleAppointmentParams params) ;
+  Future<void> cancelAppointment(String id) ;
 }
--- lib/features/book_appointment/data/data_source/remote_data_source/appointment_remote_data_source_impl.dart
+++ lib/features/book_appointment/data/data_source/remote_data_source/appointment_remote_data_source_impl.dart
@@ -1,15 +1,29 @@
 import 'package:doctor_app/features/book_appointment/domain/entities/appointment_params.dart';
 
 import '../../../../../core/services/supa_base_service/book_appointment_service.dart';
+import '../../model/appointment_model.dart';
 import 'appointment_remote_data_source.dart';
 
-class AppointmentRemoteDataSourceImpl  implements AppointmentRemoteDataSource{
+class AppointmentRemoteDataSourceImpl implements AppointmentRemoteDataSource {
   final BookAppointmentService bookAppointmentService;
-  AppointmentRemoteDataSourceImpl( this.bookAppointmentService);
+  AppointmentRemoteDataSourceImpl(this.bookAppointmentService);
   @override
-  Future<void> createAppointment(AppointmentParams params)async {
+  Future<void> createAppointment(AppointmentParams params) async {
     await bookAppointmentService.createAppointment(params);
   }
 
+  @override
+  Future<List<AppointmentModel>> getAppointment() async {
+    return await bookAppointmentService.getAppointment();
+  }
 
+  @override
+  Future<void> rescheduleAppointment(RescheduleAppointmentParams params) async {
+    await bookAppointmentService.rescheduleAppointment(params);
+  }
+
+  @override
+  Future<void> cancelAppointment(String id) async {
+    await bookAppointmentService.cancelAppointment(id);
+  }
 }
