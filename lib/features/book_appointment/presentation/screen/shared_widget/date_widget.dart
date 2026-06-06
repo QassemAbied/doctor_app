42d3b0baff31b7e6958d6e126f98b5c3b8adbd28
qassem abied <qassemab1@gmail.com>
2026-06-07 00:47:15 +0300

    add date widget to book appointment screen

---
+++ lib/features/book_appointment/presentation/screen/shared_widget/date_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DateWidget extends StatelessWidget {
+  const DateWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
