2953f65fe2328f4e96f7018af4f72953ba320044
qassem abied <qassemab1@gmail.com>
2026-06-07 00:48:16 +0300

    add cancelled or completed appointment widget for appointment status display

---
+++ lib/features/book_appointment/presentation/screen/my_appointment/widget/cancelled_or_completed_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../../../../../core/common_widgets/custom_doctor_data.dart';
+import '../../../../../../core/utils/spacing.dart';
+import '../../../../domain/entities/appointment_entity.dart';
+
+class CancelledOrCompletedWidget extends StatelessWidget {
+  final List<AppointmentEntity> appointments;
+  final String status;
+  final Color colorStatus;
+  const CancelledOrCompletedWidget({super.key, required this.appointments, required this.status, required this.colorStatus});
+
+  @override
+  Widget build(BuildContext context) {
+    return  ListView.builder(
+        itemCount: appointments.length,
+        itemBuilder:(context, index) {
+          return Column(
+            children: [
+              CustomDoctorsDataWidget(
+                status: status,
+                colorStatus: colorStatus,
+                showStatus: true,
+                showIconMessage: true,
+                doctor: appointments[index].doctor,
+                day: DateFormat(
+                  'EEEE, dd MMM',
+                ).format(DateTime.parse(appointments[index].appointmentDate,)),
+                time: appointments[index].appointmentTime,
+
+                notShowRate: false,
+              ),
+              verticalSpace(20),
+
+            ],
+          );
+        }
+    );
+  }
+}
