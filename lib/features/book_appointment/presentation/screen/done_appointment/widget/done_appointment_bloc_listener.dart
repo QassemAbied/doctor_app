c6506c1120e1ab4a39562c4a4b91952025957c32
qassem abied <qassemab1@gmail.com>
2026-06-07 00:43:42 +0300

    refactor done appointment bloc listener to use custom dialog widgets

--- lib/features/book_appointment/presentation/screen/done_appointment/widget/done_appointment_bloc_listener.dart
+++ lib/features/book_appointment/presentation/screen/done_appointment/widget/done_appointment_bloc_listener.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../../../core/common_widgets/dialog/custom_dialog_widget.dart';
 import '../../../../../../core/utils/app_router/routes.dart';
 import '../../../../../../core/utils/extension.dart';
 import '../../../controller/book_appointment_cubit.dart';
 import '../../../controller/book_appointment_state.dart';
 
 class DoneAppointmentBlocListener extends StatelessWidget {
-  const DoneAppointmentBlocListener({super.key, });
+  const DoneAppointmentBlocListener({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocListener<BookAppointmentCubit, BookAppointmentState>(
       listenWhen: (previous, current) =>
-          current is BookAppointmentLoading || current is BookAppointmentSuccess || current is BookAppointmentFailure,
+          current is BookAppointmentLoading ||
+          current is BookAppointmentSuccess ||
+          current is BookAppointmentFailure,
       listener: (context, state) {
         if (state is BookAppointmentLoading) {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return Center(child: CircularProgressIndicator());
-            },
-          );
+          CustomDialogWidget.circleDialog(context);
         }
         if (state is BookAppointmentSuccess) {
           context.pop();
@@ -28,12 +26,7 @@ class DoneAppointmentBlocListener extends StatelessWidget {
         }
         if (state is BookAppointmentFailure) {
           context.pop();
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(content: Text(state.message));
-            },
-          );
+          CustomDialogWidget.errorDialog(context, state.message);
         }
       },
       child: SizedBox.shrink(),
