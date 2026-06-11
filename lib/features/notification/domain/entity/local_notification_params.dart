f03a126dd664521ec4ebbe58c0f857f388f761be
qassem abied <qassemab1@gmail.com>
2026-06-11 21:05:44 +0300

    add local notification params entity for notification management

---
+++ lib/features/notification/domain/entity/local_notification_params.dart
@@ -0,0 +1,10 @@
+class LocalNotificationParams {
+  final String title;
+  final String body;
+  final bool isRead;
+
+  LocalNotificationParams({
+    required this.title,
+    required this.body,
+    required this.isRead});
+}
