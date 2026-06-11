1cfff3ecc65480f0589245629a5a17ca779ae453
qassem abied <qassemab1@gmail.com>
2026-06-11 21:04:09 +0300

    add chat model for chat data representation

---
+++ lib/features/chat/data/model/chat_model.dart
@@ -0,0 +1,27 @@
+
+class ChatModel {
+
+final String receiverId;
+
+final String name;
+
+final String image;
+
+final String lastMessage;
+
+final String time;
+
+ChatModel({
+
+required this.receiverId,
+
+required this.name,
+
+required this.image,
+
+required this.lastMessage,
+
+required this.time,
+});
+}
+
