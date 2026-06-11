5b85a38948a60141f47c982e5d78d01aa861b600
qassem abied <qassemab1@gmail.com>
2026-06-11 21:00:24 +0300

    initialize time zones and local notifications in main user entry point

--- lib/main_user.dart
+++ lib/main_user.dart
@@ -3,16 +3,19 @@ import 'package:doctor_app/core/services/shared_pref/shared_pref_helpers.dart';
 import 'package:doctor_app/core/services/deep_links_services.dart';
 import 'package:flutter/material.dart';
 
+import 'core/services/notification/local_notification.dart';
 import 'core/utils/app_router/router_app.dart';
 import 'core/utils/extension.dart';
 import 'core/helpers/supabase_helper.dart';
 import 'core/utils/di/injection_container.dart';
 import 'features/doctor_app.dart';
-
+import 'package:timezone/data/latest.dart' as tz;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  tz.initializeTimeZones();
   await SupAbaseHelper.init();
+ await LocalNotification.init();
+
   await init();
  await DeepLinkService().init();
   await checkLoggedInUser();
