fc09ec622f001aaa41b56748a851eabf1ff67410
qassem abied <qassemab1@gmail.com>
2026-06-11 20:59:48 +0300

    register chat and notification feature dependencies in injection container

--- lib/core/utils/di/injection_container.dart
+++ lib/core/utils/di/injection_container.dart
@@ -1,5 +1,6 @@
 import 'package:doctor_app/core/services/location/location_service.dart';
 import 'package:doctor_app/core/services/supa_base_service/book_appointment_service.dart';
+import 'package:doctor_app/core/services/supa_base_service/supa_base_chat_service.dart';
 import 'package:doctor_app/core/services/supa_base_service/supa_base_doctor_details_service.dart';
 import 'package:doctor_app/core/services/supa_base_service/supa_base_doctor_specialization_services.dart';
 import 'package:doctor_app/features/auth/domain/use_case/get_user_uescase.dart';
@@ -9,6 +10,7 @@ import 'package:doctor_app/features/book_appointment/data/data_source/remote_dat
 import 'package:doctor_app/features/book_appointment/domain/repository.dart';
 import 'package:doctor_app/features/book_appointment/domain/use_case/book_appointment_usecase.dart';
 import 'package:doctor_app/features/book_appointment/presentation/controller/book_appointment_cubit.dart';
+import 'package:doctor_app/features/chat/presentation/controller/chat_cubit.dart';
 import 'package:doctor_app/features/details_doctor/data/data_source/remote_data_source/details_remote_data_source.dart';
 import 'package:doctor_app/features/details_doctor/domain/repository.dart';
 import 'package:doctor_app/features/details_doctor/domain/use_case/get_doctor_by_id_usecase.dart';
@@ -22,6 +24,9 @@ import 'package:doctor_app/features/home/domain/use_case/get_specialization_usec
 import 'package:doctor_app/features/home/presentation/controller/doctor/doctors_cubit.dart';
 import 'package:doctor_app/features/home/presentation/controller/recommendation/recommendation_cubit.dart';
 import 'package:doctor_app/features/home/presentation/controller/specialization/specialization_cubit.dart';
+import 'package:doctor_app/features/notification/data/data_source/remote_data_source/local_notification_remote_data_source.dart';
+import 'package:doctor_app/features/notification/domain/use_case/add_local_notification_usecase.dart';
+import 'package:doctor_app/features/notification/domain/use_case/get_local_notification_usecase.dart';
 import 'package:get_it/get_it.dart';
 import '../../../features/auth/data/data_source/auth_remote_data_source.dart';
 import '../../../features/auth/data/data_source/auth_remote_data_source_impl.dart';
@@ -37,9 +42,21 @@ import '../../../features/book_appointment/data/repository_impl.dart';
 import '../../../features/book_appointment/domain/use_case/cancel_appointment_usecase.dart';
 import '../../../features/book_appointment/domain/use_case/reschedule_appointment_uescase.dart';
 import '../../../features/book_appointment/domain/use_case/my_appointment_usecase.dart';
+import '../../../features/chat/data/data_source/remote_data_source/chat_remote_data_source.dart';
+import '../../../features/chat/data/data_source/remote_data_source/chat_remote_data_source_impl.dart';
+import '../../../features/chat/data/repository_impl.dart';
+import '../../../features/chat/domain/repository.dart';
+import '../../../features/chat/domain/use_case/get_all_message_usecase.dart';
+import '../../../features/chat/domain/use_case/get_message_usecase.dart';
+import '../../../features/chat/domain/use_case/send_message.dart';
 import '../../../features/details_doctor/data/data_source/remote_data_source/details_remote_data_source_impl.dart';
 import '../../../features/details_doctor/data/repository_impl.dart';
+import '../../../features/notification/data/data_source/remote_data_source/local_notification_remote_data_source_impl.dart';
+import '../../../features/notification/data/repository_impl.dart';
+import '../../../features/notification/domain/repository.dart';
+import '../../../features/notification/presentation/controller/notification_cubit.dart';
 import '../../services/supa_base_service/supa_base_auth_service.dart';
+import '../../services/supa_base_service/supa_base_notification_service.dart';
 
 final sl = GetIt.instance;
 
@@ -97,4 +114,32 @@ Future<void> init() async {
     () => AppointmentRepositoryImpl(sl()),
   );
   sl.registerLazySingleton(() => BookAppointmentService());
+
+  sl.registerFactory(() => NotificationCubit(sl(), sl(), sl()));
+
+  sl.registerLazySingleton(() => AddLocalNotificationUseCase(sl()));
+  sl.registerLazySingleton(() => GetLocalNotificationUseCase(sl()));
+  sl.registerLazySingleton<LocalNotificationRepository>(
+    () => LocalNotificationRepositoryImpl(sl()),
+  );
+  sl.registerLazySingleton<LocalNotificationRemoteDataSource>(
+    () => LocalNotificationRemoteDataSourceImpl(sl()),
+
+  );
+  sl.registerLazySingleton(() => SupABaseNotificationService());
+
+
+  sl.registerFactory(() => ChatCubit(sl(), sl(),sl()));
+  sl.registerLazySingleton(() => GetAllChatsUseCase(sl()));
+  sl.registerLazySingleton(() => GetMessagesUseCase(sl()));
+  sl.registerLazySingleton(() => SendMessageUseCase(sl()));
+  sl.registerLazySingleton<ChatRepository>(
+        () => ChatRepositoryImpl(sl()),
+  );
+  sl.registerLazySingleton<ChatRemoteDataSource>(
+        () => ChatRemoteDataSourceImpl(sl()),
+
+  );
+  sl.registerLazySingleton(() => SupABaseChatService());
+
 }
