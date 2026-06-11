295e546e1ac21ece595c36adaf47c96ddab1658c
qassem abied <qassemab1@gmail.com>
2026-06-11 20:59:56 +0300

    add notification navigation and logout dialog to profile list titles

--- lib/features/profile/screens/profile/widget/list_title_list_widget.dart
+++ lib/features/profile/screens/profile/widget/list_title_list_widget.dart
@@ -1,4 +1,6 @@
+import 'package:doctor_app/core/constants/app_svgs.dart';
 import 'package:doctor_app/core/theming/app_color.dart';
+import 'package:doctor_app/core/theming/app_styles.dart';
 import 'package:doctor_app/features/auth/presentation/controller/auth_cubit.dart';
 import 'package:doctor_app/features/auth/presentation/controller/auth_state.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +9,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../core/utils/app_router/routes.dart';
 import '../../../../../core/utils/extension.dart';
 import '../../../../../core/utils/spacing.dart';
+import '../../../../notification/presentation/screen/notification_screen.dart';
 import 'list_title_widget.dart';
 
 class ListTitleListWidget extends StatelessWidget {
@@ -42,19 +45,66 @@ class ListTitleListWidget extends StatelessWidget {
         Divider(color: ColorManager.grey20, height: 1),
         verticalSpace(10),
         ListTitleWidget(
-          image: 'assets/svgs/my_appointment_icon.svg',
-          text: 'My Test & Diagnostic',
+          image: AppSvgs.myAppointment,
+          text: 'Notifications',
           onTap: () {
-            context.pushNamed('/myAppointmentsScreen');
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const NotificationScreen()),
+            );
           },
         ),
         verticalSpace(10),
         Divider(color: ColorManager.grey20, height: 1),
         verticalSpace(10),
         ListTitleWidget(
-          image: 'assets/svgs/logout_icon.svg',
-          text: 'Payment',
-          onTap: () {},
+          image: AppSvgs.logout,
+          text: 'Logout',
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text(
+                    'Logout',
+                    style: AppTextStyle.styleBold16(
+                      ColorManager.textPrimary,
+                      context,
+                    ),
+                  ),
+                  content: Text(
+                    'You’ll need to enter your email and password next time you want to login',
+                    style: AppTextStyle.styleMedium14(
+                      ColorManager.textSecondary,
+                      context,
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        context.pop();
+                      },
+                      child: const Text('Cancel'),
+                    ),
+
+                    TextButton(
+                      onPressed: () {
+                        context.pop();
+                        context.read<AuthCubit>().signOut();
+                      },
+                      child: Text(
+                        'Logout',
+                        style: AppTextStyle.styleBold16(
+                          ColorManager.fillRed,
+                          context,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
         ),
         verticalSpace(10),
         Divider(color: ColorManager.grey20, height: 1),
