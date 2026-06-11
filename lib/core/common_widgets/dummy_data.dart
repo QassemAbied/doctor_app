f3dfe1535afaa390d6eb545e8eab2c881b3b484d
qassem abied <qassemab1@gmail.com>
2026-06-11 20:58:23 +0300

    add fake notifications data to dummy data helper

--- lib/core/common_widgets/dummy_data.dart
+++ lib/core/common_widgets/dummy_data.dart
@@ -1,31 +1,29 @@
 import '../../features/book_appointment/domain/entities/appointment_entity.dart';
 import '../../features/home/domain/entities/doctor_entity.dart';
 import '../../features/home/domain/entities/specialization_entity.dart';
+import '../../features/notification/domain/entity/local_notification_entities.dart';
 
 class DummyData {
-
-  static List<SpecializationEntity>
-  fakeSpecializations = List.generate(
-
+  static List<SpecializationEntity> fakeSpecializations = List.generate(
     6,
 
-        (index) =>
-        SpecializationEntity.fake(),
+    (index) => SpecializationEntity.fake(),
   );
 
-  static List<DoctorEntity>
-  fakeDoctors = List.generate(
+  static List<DoctorEntity> fakeDoctors = List.generate(
     6,
 
-        (index) => DoctorEntity.fake(),
+    (index) => DoctorEntity.fake(),
   );
 
   static DoctorEntity fakeDetailsDoctor = DoctorEntity.fake();
-  static List<AppointmentEntity>
-  fakeAppointments = List.generate(
+  static List<AppointmentEntity> fakeAppointments = List.generate(
     6,
-        (index) => AppointmentEntity.fake(),
+    (index) => AppointmentEntity.fake(),
   );
+  static List<NotificationEntity> fakeNotifications = List.generate(
+    6,
 
-
+    (index) => NotificationEntity.fake(),
+  );
 }
