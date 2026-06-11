532d02606e1d518d20841cdef25f97af646fdae1
qassem abied <qassemab1@gmail.com>
2026-06-11 21:03:43 +0300

    add chat entity for representing chat messages and metadata

---
+++ lib/features/chat/domain/entities/chat_entity.dart
@@ -0,0 +1,23 @@
+class ChatEntity {
+  final String receiverId;
+
+  final String name;
+
+  final String image;
+
+  final String lastMessage;
+
+  final String time;
+
+  ChatEntity({
+    required this.receiverId,
+
+    required this.name,
+
+    required this.image,
+
+    required this.lastMessage,
+
+    required this.time,
+  });
+}
