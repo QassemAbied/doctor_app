22774cd0f5343eaa0bede35235e5ecdc615518c1
qassem abied <qassemab1@gmail.com>
2026-06-11 21:06:10 +0300

    add message mapper for converting message model to entity

---
+++ lib/features/chat/domain/mapper/message_mapper.dart
@@ -0,0 +1,20 @@
+import '../../data/model/message_model.dart';
+import '../entities/message_entity.dart';
+
+extension MessageModelMapper on MessageModel {
+  MessageEntity toEntity() {
+    return MessageEntity(
+      id: id,
+
+      senderId: senderId,
+
+      receiverId: receiverId,
+
+      message: message,
+
+      image: image,
+
+      createdAt: createdAt,
+    );
+  }
+}
