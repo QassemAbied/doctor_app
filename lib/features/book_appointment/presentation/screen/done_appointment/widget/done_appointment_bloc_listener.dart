a25afcf6df7ac4e77b4bc629461a7424599f578c
qassem abied <qassemab1@gmail.com>
2026-06-15 17:13:47 +0300

    remove unused notification imports and debug print in done appointment bloc listener

--- lib/features/book_appointment/presentation/screen/done_appointment/widget/done_appointment_bloc_listener.dart
+++ lib/features/book_appointment/presentation/screen/done_appointment/widget/done_appointment_bloc_listener.dart
@@ -3,8 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../../core/common_widgets/dialog/custom_dialog_widget.dart';
 import '../../../../../../core/utils/app_router/routes.dart';
 import '../../../../../../core/utils/extension.dart';
-import '../../../../../notification/domain/entity/local_notification_params.dart';
-import '../../../../../notification/presentation/controller/notification_cubit.dart';
 import '../../../controller/book_appointment_cubit.dart';
 import '../../../controller/book_appointment_state.dart';
 
@@ -19,9 +17,7 @@ class DoneAppointmentBlocListener extends StatelessWidget {
           current is BookAppointmentSuccess ||
           current is BookAppointmentFailure,
       listener: (context, state) {
-        print(
-          'STATE => $state',
-        );
+
         if (state is BookAppointmentLoading) {
           CustomDialogWidget.circleDialog(context);
         }
