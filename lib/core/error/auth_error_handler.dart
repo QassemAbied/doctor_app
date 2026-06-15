e945524ba1912dbc26e8a3aa2a309a8518f7854a
qassem abied <qassemab1@gmail.com>
2026-06-15 17:10:26 +0300

    refactor authentication error handler to simplify error cases and update default failure message

--- lib/core/error/auth_error_handler.dart
+++ lib/core/error/auth_error_handler.dart
@@ -1,6 +1,5 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'error.dart';
-
 class AuthErrorHandler {
   static Failure handle(dynamic error) {
     if (error is AuthApiException) {
@@ -11,26 +10,44 @@ class AuthErrorHandler {
         case 'invalid_credentials':
           return Failure('Email or password is incorrect.');
 
-        case 'over_email_send_rate_limit':
-          return Failure('Too many requests. Please try again later.');
-
-        case 'user_already_exists':
-          return Failure('User already exists.');
-
-        case 'weak_password':
-          return Failure('Password is too weak.');
-
-        case 'otp_expired':
-          return Failure('Reset password link expired.');
-
-        case 'access_denied':
-          return Failure('Access denied.');
-
         default:
           return Failure(error.message);
       }
     }
 
-    return Failure('Unexpected error occurred.');
+    return Failure(error.toString());
   }
 }
+// class AuthErrorHandler {
+//   static Failure handle(dynamic error) {
+//     if (error is AuthApiException) {
+//       switch (error.code) {
+//         case 'email_address_invalid':
+//           return Failure('Invalid email address.');
+//
+//         case 'invalid_credentials':
+//           return Failure('Email or password is incorrect.');
+//
+//         case 'over_email_send_rate_limit':
+//           return Failure('Too many requests. Please try again later.');
+//
+//         case 'user_already_exists':
+//           return Failure('User already exists.');
+//
+//         case 'weak_password':
+//           return Failure('Password is too weak.');
+//
+//         case 'otp_expired':
+//           return Failure('Reset password link expired.');
+//
+//         case 'access_denied':
+//           return Failure('Access denied.');
+//
+//         default:
+//           return Failure(error.message);
+//       }
+//     }
+//
+//     return Failure('Unexpected error occurred.');
+//   }
+// }
