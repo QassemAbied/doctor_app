5720e25dd4512c84efe1070c1915ad7c2ed0e95c
qassem abied <qassemab1@gmail.com>
2026-06-18 06:30:56 +0300

    add image size validation and snackbar notification to image picker service

--- lib/core/services/image/image_picker_service.dart
+++ lib/core/services/image/image_picker_service.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
+import '../../common_widgets/dialog/custom_dialog_widget.dart';
+
 class ImagePickerService {
-  Future<File?> pickImage() async {
+  static Future<File?> pickImage(BuildContext context) async {
     final picker = ImagePicker();
 
     final image = await picker.pickImage(source: ImageSource.gallery);
@@ -11,6 +14,13 @@ class ImagePickerService {
     if (image == null) return null;
 
     if (await image.length() > 1024 * 1024) {
+      if (context.mounted) {
+        CustomDialogWidget.showSnackBar(
+          context,
+          'Image size must not exceed 1MB',
+        );
+      }
+
       return null;
     }
 
