829e2a506efed9f007093f82e2c01f8f81e33bfd
qassem abied <qassemab1@gmail.com>
2026-06-27 12:25:29 +0300

    add doctor header widget for doctor profile display

---
+++ lib/features/doctor_profile/presentation/screen/doctor_home/widget/doctor_header_widget.dart
@@ -0,0 +1,70 @@
+import 'package:doctor_app/core/common_widgets/custom_image_or_name_widget.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../../core/theming/app_color.dart';
+import '../../../../../../core/theming/app_styles.dart';
+import '../../../../../../core/utils/spacing.dart';
+import '../../../../../home/domain/entities/doctor_entity.dart';
+
+class DoctorHeaderWidget extends StatelessWidget {
+  final DoctorEntity doctor;
+
+  const DoctorHeaderWidget({super.key, required this.doctor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          CustomImageOrNameWidget(name: doctor.name, image: doctor.image),
+
+          horizontalSpace(16),
+
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Dr. ${doctor.name}',
+                  style: AppTextStyle.styleBold18(
+                    ColorManager.whiteColor,
+                    context,
+                  ),
+                ),
+
+                verticalSpace(6),
+
+                Text(
+                  doctor.specializationName,
+                  style: AppTextStyle.styleSemiBold16(
+                    ColorManager.whiteColor,
+                    context,
+                  ),
+                ),
+
+                verticalSpace(8),
+
+                Row(
+                  children: [
+                    Icon(Icons.star, color: Colors.amber, size: 18),
+
+                    horizontalSpace(4),
+
+                    Text(
+                      doctor.rating.toString(),
+                      style: AppTextStyle.styleMedium16(
+                        ColorManager.whiteColor,
+                        context,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
