2337e113161350e3958e8699279dbaacff689591
qassem abied <qassemab1@gmail.com>
2026-06-27 12:05:00 +0300

    refactor application startup logic by introducing AppStartupHelper and removing redundant auth checks from main files

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
-import 'core/services/shared_pref/shared_pref_keys.dart';
-import 'core/services/shared_pref/shared_pref_helpers.dart';
 import 'core/helpers/supabase_helper.dart';
 import 'core/utils/app_router/router_app.dart';
 import 'features/doctor_app.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await SupAbaseHelper.init();
-  await checkLoggedInUser();
   runApp(DoctorApp(routerApp: RouterApp()));
 }
 
-Future checkLoggedInUser()async{
-  String? userToken = await SharedPrefHelper.getString(SharedPrefKeys.userToken);
 
-  if(  userToken==''&& userToken.isNotEmpty){
-    isLoggedInUser=true;
-  }else{
-    isLoggedInUser=false;
-  }
-}
--- lib/main_user.dart
+++ lib/main_user.dart
@@ -1,14 +1,12 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:doctor_app/core/services/shared_pref/shared_pref_keys.dart';
-import 'package:doctor_app/core/services/shared_pref/shared_pref_helpers.dart';
 import 'package:doctor_app/core/services/deep_links_services.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'core/helpers/app_startup_helper.dart';
 import 'core/services/notification/local_notification.dart';
 import 'core/utils/app_router/router_app.dart';
-import 'core/utils/extension.dart';
 import 'core/helpers/supabase_helper.dart';
 import 'core/utils/di/injection_container.dart';
 import 'features/doctor_app.dart';
@@ -26,8 +24,7 @@ void main() async {
   await Stripe.instance.applySettings();
   await init();
   await DeepLinkService().init();
-  await checkLoggedInUser();
-  await isOnBoarding();
+  await AppStartupHelper.init();
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
@@ -35,25 +32,4 @@ void main() async {
       builder: (context) => DoctorApp(routerApp: RouterApp()),
     ),
   );
-}
-
-Future isOnBoarding() async {
-  bool? isOnBoarding = await SharedPrefHelper.getBool(
-    SharedPrefKeys.isOnBoarding,
-  );
-  if (isOnBoarding == true) {
-    checkIsOnBoarding = false;
-  } else {
-    checkIsOnBoarding = true;
-  }
-}
-
-Future checkLoggedInUser() async {
-  String? userToken = await SharedPrefHelper.getString(SharedPrefKeys.userId);
-
-  if (!userToken.isNullOrEmpty()) {
-    isLoggedInUser = true;
-  } else {
-    isLoggedInUser = false;
-  }
 }
