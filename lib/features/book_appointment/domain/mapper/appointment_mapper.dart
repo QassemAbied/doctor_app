f6b81b14ea69b685317c9d829185f6e6a64c6153
qassem abied <qassemab1@gmail.com>
2026-06-27 12:06:48 +0300

    update appointment mapper to include user entity mapping and date parsing

--- lib/features/book_appointment/domain/mapper/appointment_mapper.dart
+++ lib/features/book_appointment/domain/mapper/appointment_mapper.dart
@@ -1,33 +1,38 @@
+import '../../../auth/domain/mapper/user_mapper.dart';
 import '../../../home/domain/mapper/doctor_mapper.dart';
 import '../../data/model/appointment_model.dart';
 import '../entities/appointment_entity.dart';
 
 class AppointmentMapper {
-  static AppointmentEntity toEntity(AppointmentModel model) {
+  static AppointmentEntity toEntity(
+      AppointmentModel model,
+      ) {
     return AppointmentEntity(
       id: model.id,
-
       doctorId: model.doctorId,
-
       userId: model.userId,
-
-      appointmentDate: model.appointmentDate,
-
+      appointmentDate: DateTime.parse(
+        model.appointmentDate,
+      ),
       appointmentTime: model.appointmentTime,
-
       appointmentType: model.appointmentType,
-
       paymentMethod: model.paymentMethod,
-
       price: model.price,
-
       status: model.status,
-
-      doctor: DoctorMapper.toEntity(model.doctor),
+      doctor: DoctorMapper.toEntity(
+        model.doctor,
+      ),
+      user: model.user == null
+          ? null
+          : UserMapper.toEntity(model.user!),
     );
   }
 
-  static List<AppointmentEntity> toEntityList(List<AppointmentModel> models) {
-    return models.map((e) => toEntity(e)).toList();
+  static List<AppointmentEntity> toEntityList(
+      List<AppointmentModel> models,
+      ) {
+    return models
+        .map(toEntity)
+        .toList();
   }
 }
