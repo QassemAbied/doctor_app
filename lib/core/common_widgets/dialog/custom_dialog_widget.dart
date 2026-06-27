4f80549ea57f56cf35deadc5b24ee6aec6b4a35b
qassem abied <qassemab1@gmail.com>
2026-06-27 12:07:15 +0300

    add logout dialog widget with sign out functionality

--- lib/core/common_widgets/dialog/custom_dialog_widget.dart
+++ lib/core/common_widgets/dialog/custom_dialog_widget.dart
@@ -1,6 +1,11 @@
 import 'package:doctor_app/core/theming/app_color.dart';
 import 'package:doctor_app/core/theming/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../features/auth/presentation/controller/auth_cubit.dart';
+import '../../utils/app_router/routes.dart';
+import '../../utils/extension.dart';
 
 class CustomDialogWidget {
   static Future<void> circleDialog(BuildContext context) async {
@@ -39,6 +44,47 @@ class CustomDialogWidget {
           style: AppTextStyle.styleBold16(ColorManager.whiteColor, context),
         ),
       ),
+    );
+  }
+
+  static Future logOutDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(
+            'Logout',
+            style: AppTextStyle.styleBold16(ColorManager.textPrimary, context),
+          ),
+          content: Text(
+            'You’ll need to enter your email and password next time you want to login',
+            style: AppTextStyle.styleMedium14(
+              ColorManager.textSecondary,
+              context,
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                context.pop();
+              },
+              child: const Text('Cancel'),
+            ),
+
+            TextButton(
+              onPressed: () async {
+                context.pop();
+                context.read<AuthCubit>().signOut();
+                await context.pushNamedAndRemoveUntil(Routes.loginScreen);
+              },
+              child: Text(
+                'Logout',
+                style: AppTextStyle.styleBold16(ColorManager.fillRed, context),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
