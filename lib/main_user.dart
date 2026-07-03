1b37f2032ff10335ef2bd824f32987b65bc08005
qassem abied <qassemab1@gmail.com>
2026-07-03 15:44:34 +0300

    initialize firebase and fcm notification services in main user entry point

--- lib/main_user.dart
+++ lib/main_user.dart
@@ -1,20 +1,26 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:doctor_app/core/services/deep_links_services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'core/helpers/app_startup_helper.dart';
+import 'core/services/notification/fcm_notification.dart';
 import 'core/services/notification/local_notification.dart';
+import 'core/services/supa_base_service/supa_base_auth_service.dart';
 import 'core/utils/app_router/router_app.dart';
 import 'core/helpers/supabase_helper.dart';
 import 'core/utils/di/injection_container.dart';
 import 'features/doctor_app.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await dotenv.load(fileName: ".env");
+
   tz.initializeTimeZones();
   await SupAbaseHelper.init();
   if (!kIsWeb) {
@@ -25,11 +31,9 @@ void main() async {
   await init();
   await DeepLinkService().init();
   await AppStartupHelper.init();
-  runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FcmNotificationService.init();
+  await SupAbaseAuthService.saveToken();
 
-      builder: (context) => DoctorApp(routerApp: RouterApp()),
-    ),
-  );
+  runApp(DoctorApp(routerApp: RouterApp()));
 }
