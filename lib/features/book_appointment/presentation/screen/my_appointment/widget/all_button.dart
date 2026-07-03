a003506a0a54fa989242ba24751b5e1201d43520
qassem abied <qassemab1@gmail.com>
2026-07-03 15:42:55 +0300

    add notification trigger for appointment cancellation in my appointments widget

--- lib/features/book_appointment/presentation/screen/my_appointment/widget/all_button.dart
+++ lib/features/book_appointment/presentation/screen/my_appointment/widget/all_button.dart
@@ -4,24 +4,40 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../../core/common_widgets/custom_elevated_botton.dart';
 import '../../../../../../core/utils/spacing.dart';
+import '../../../../../notification/domain/entity/local_notification_params.dart';
+import '../../../../../notification/presentation/controller/notification_cubit.dart';
+import '../../../../domain/entities/appointment_entity.dart';
 
 class AllButtonInMyAppointments extends StatelessWidget {
   final String id;
   final VoidCallback onPressed;
+  final AppointmentEntity appointments;
   const AllButtonInMyAppointments({
     super.key,
     required this.id,
-    required this.onPressed,
+    required this.onPressed, required this.appointments,
   });
 
   @override
   Widget build(BuildContext context) {
+    final notificationCubit = context.read<NotificationCubit>();
     return Row(
       children: [
         Expanded(
           child: OutlinedButton(
-            onPressed: () {
+            onPressed: ()async {
               context.read<BookAppointmentCubit>().cancelAppointment(id);
+              await notificationCubit.addShowNotification(
+                params: LocalNotificationParams(
+                  title: 'New appointment with Cancel',
+                  body:
+                  'Dear ${appointments.doctor.name} '
+                      '${appointments.user?.name??''} Cancel an appointment with you',
+                  isRead: false,
+                  id: appointments.doctor.userId??"",
+                ),
+                doctorUserId:appointments.doctor.userId??"",
+              );
             },
             child: FittedBox(
               fit: BoxFit.scaleDown,
